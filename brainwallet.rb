9317fc9f92b0c509bbb43a79cdae32bba3dd3103
Martin Ankerl <martin.ankerl@gmail.com>
2013-04-25 00:41:56 +0200

    cleanup

--- brainwallet.rb
+++ brainwallet.rb
@@ -12,16 +12,12 @@ def base16_to_base58(addr)
 end
 
 def pwd_to_privatekey(pwd)
-    a = "\x80" + Digest::SHA256.digest(pwd)
-    b = Digest::SHA256.digest(a)
-    b = Digest::SHA256.digest(b)
-    b = b[0...4]
-    b = a + b
-    base16 = b.unpack('H*')[0]
+    h = "\x80" + Digest::SHA256.digest(pwd)
+    h = h + Digest::SHA256.digest(Digest::SHA256.digest(h))[0...4]
+    base16 = h.unpack('H*')[0]
     base16_to_base58(base16)
 end
 
-# this script just imports everything. A separate script periodically sweeps everything.
 pwd = ARGV[0]
 puts "password:    [#{pwd}]"
 puts "private key: [#{pwd_to_privatekey(pwd)}]"
