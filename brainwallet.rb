4053e044f9d0c5d6bcc4e8f5fc1c0af92bb3ea5a
martinus <martin.ankerl@gmail.com>
2013-04-25 14:40:45 +0200

    works in Ruby 1.8, works even when address is just 1

--- brainwallet.rb
+++ brainwallet.rb
@@ -2,18 +2,19 @@
 require 'digest/sha2'
 
 def base16_to_base58(addr)
+    letters = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".unpack("C*") 
     s = ""
     num = addr.to_i(16)
-    while num != 0 do
-        s += "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"[num % 58]
+    begin    
+        s += letters[num % 58].chr
         num /= 58
-    end
+    end while num != 0
     s.reverse
 end
 
 def pwd_to_privatekey(pwd)
     h = "\x80" + Digest::SHA256.digest(pwd)
-    h = h + Digest::SHA256.digest(Digest::SHA256.digest(h))[0...4]
+    h += Digest::SHA256.digest(Digest::SHA256.digest(h))[0...4]
     base16 = h.unpack('H*')[0]
     base16_to_base58(base16)
 end
